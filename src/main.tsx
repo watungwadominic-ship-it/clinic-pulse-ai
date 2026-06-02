2d3c58c8953a25350881eafdb9799d4ef0665927
watungwadominic-ship-it <watungwadominic@gmail.com>
2026-06-02 15:35:06 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics';
 import App from './App.tsx';
 import './index.css';
+
+// Initialize Vercel Analytics
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 );
